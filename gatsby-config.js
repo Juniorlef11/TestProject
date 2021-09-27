00c71f93639fc1bcb65d73976d88d1692dc2a88b
Konstantinos Lefevr <konst.lefevr@gmail.com>
2021-09-27 16:42:00 +0300

    Adding Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Smaster's Blog`,
+    description: `IT Thoughts and more...`,
+    author: `@Konstantinos Lefevr`,
+    siteUrl: `https://klefevr.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
